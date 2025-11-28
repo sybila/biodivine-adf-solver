c5da82afc885e0cec4c45f8496aca5d665e97fb4
Samuel Pastva <sam.pastva@gmail.com>
2025-11-28 22:25:17 +0100

    Add file size limit to goDiamond

--- benchmarks/bench_goDiamond.sh
+++ benchmarks/bench_goDiamond.sh
@@ -13,17 +13,17 @@ TIMEOUT=${TIMEOUT:-'10s'}
 PARALLEL=${PARALLEL:-'1'}
 
 # Time to one solution across various semantics.
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -mod
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -mod -enum
 for d in run_*/; do mv -- "$d" "results/go_diamond_2v_${d#./}"; done
 
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -adm
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -adm -enum
 for d in run_*/; do mv -- "$d" "results/go_diamond_adm_${d#./}"; done
 
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -com
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -com -enum
 for d in run_*/; do mv -- "$d" "results/go_diamond_com_${d#./}"; done
 
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -prf
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -prf -enum
 for d in run_*/; do mv -- "$d" "results/go_diamond_prf_${d#./}"; done
 
-python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -stm
+python3 ./benchmarks/bench_docker.py --docker-image $TOOL --timeout $TIMEOUT --folder $BENCHMARKS --match '.*.adf' --parallel $PARALLEL -- --count-only -stm -enum
 for d in run_*/; do mv -- "$d" "results/go_diamond_stb_${d#./}"; done
--- benchmarks/docker-goDiamond/run.sh
+++ benchmarks/docker-goDiamond/run.sh
@@ -8,11 +8,15 @@ trap 'echo "Received signal, exiting..."; exit 0' SIGINT SIGTERM
 set -x
 set -e
 
+# Note that goDiamond heavily relies on tempfiles, which can cause exhaust
+# the available drive space. To mitigate this, we only allow each invocation
+# to create files that have 4GB.
+
 if [ "$1" = "--count-only" ]; then
-    python3 /sw/diamond/diamond.py "${@:2}" | wc -l
+    (ulimit -f 4194304; python3 /sw/diamond/diamond.py "${@:2}" | wc -l)
     exit_code=${PIPESTATUS[0]}
     echo "Exit code of goDiamond: $exit_code"
     exit $exit_code
 else
-    python3 /sw/diamond/diamond.py "$@"
+    (ulimit -f 4194304; python3 /sw/diamond/diamond.py "$@")
 fi
